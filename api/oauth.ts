d214ef3f1e4ecb97d57eff2c8e1fc4fc3466bd53
TÃ¹ng <legendarytvst@gmail.com>
2026-03-02 13:54:34 +0700

    Revert "Add Vercel deployment config with serverless API functions"
    This reverts commit 1d7e0207e8c70206c42d36feb97a983cf2db5903.

--- api/oauth.ts
+++
@@ -1,9 +0,0 @@
-import "dotenv/config";
-import express from "express";
-import { registerOAuthRoutes } from "../../server/_core/oauth";
-
-const app = express();
-app.use(express.json());
-registerOAuthRoutes(app);
-
-export default app;
